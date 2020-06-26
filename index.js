348a55c67a47b75b9740c5a3c29d7d30f85390b8
Rogério Araújo <rogerio.araujo@mail.com>
2020-06-25 23:36:58 -0300

    feat: added cents/money formatter to currency

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var { currencies } = require('./currency');
+
 function MoneyFormating(currency) {
   this.currency = currency;
 }
@@ -31,9 +33,48 @@ MoneyFormating.prototype.moneyToCents = function moneyToCents(amount) {
  */
 MoneyFormating.prototype.centsToMoney = function centsToMoney(cents) {
   if (isNaN(cents) || cents == null) {
-    throw new Error('Cents must be a value type Number or String without periods or commas.');
+    throw new Error(
+      'Cents must be a value type Number or String without periods or commas.'
+    );
   }
   return Number(cents) / 100;
+};
+
+/**
+ * Convert cents value into currency formated value.
+ *
+ * @since 0.1.1
+ * @param  {String} amount Money value
+ * @returns {String} Returns the money formatted.
+ * @public
+ */
+MoneyFormating.prototype.moneyToCurrency = function moneyToCurrency(
+  cents,
+  currency = ''
+) {
+  if (typeof cents == 'number' || cents == null || cents == undefined) {
+    throw new Error('Cents must be a value of type String.');
+  }
+
+  var value = '',
+    regex = /(\d)(?=(\d\d\d)+(?!\d))/g,
+    precision = 2,
+    thousands = '.',
+    decimal = ',',
+    currency =
+      typeof currencies[currency.toUpperCase()] === 'undefined'
+        ? ''
+        : currencies[currency.toUpperCase()].symbol + ' ';
+
+  cents = replaceSymbols(cents);
+  cents = (cents / 100.0).toFixed(precision);
+
+  var parts = cents.split('.'),
+    money = parts[0].replace(regex, '$1' + thousands),
+    cents = parts[1] ? decimal + parts[1] : '';
+
+  value = currency + money + cents;
+  return value;
 };
 
 /**
@@ -43,6 +84,20 @@ MoneyFormating.prototype.centsToMoney = function centsToMoney(cents) {
  * @private
  */
 function replaceSymbols(amount) {
-  return amount.replace(/[$,R.]/g, '');
+  return amount.toString().replace(/[$,R. ]/g, '');
 }
+
+/**
+ * Returns a default value provide, if option is undefiend.
+ *
+ * @since 0.1.1
+ * @param {*} opt The provide option value
+ * @param {*} def The provide default value
+ * @returns {*} The default value or option if not undefiend
+ *
+ */
+function defaultOption(opt, def) {
+  return typeof opt == 'undefined' ? def : opt;
+}
+
 module.exports = MoneyFormating;
